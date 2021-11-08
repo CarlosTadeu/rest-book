fe5b8d75a45ed5a466d5d8ac3ff9f5098287b6f9
Carlos Tadeu <carlos.santos@opus-software.com.br>
2021-11-08 14:50:05 -0300

    Initial commit

---
+++ src/main/java/org/ctadeu/quarkus/starting/BookResource.java
@@ -0,0 +1,44 @@
+package org.ctadeu.quarkus.starting;
+
+import org.jboss.logging.Logger;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+import java.util.Optional;
+
+@Path("/api/books")
+@Produces(MediaType.APPLICATION_JSON)
+public class BookResource {
+
+    @Inject
+    BookRepository bookRepository;
+
+    @Inject
+    Logger logger;
+
+    @GET
+    public List<Book> getAllBooks() {
+        logger.info("Return all books");
+        return bookRepository.getAllBooks();
+    }
+
+    @GET
+    @Path("/count")
+    @Produces(MediaType.TEXT_PLAIN)
+    public int countAllBooks() {
+        logger.info("Return the number of books");
+        return bookRepository.getAllBooks().size();
+    }
+
+    @GET
+    @Path("{id}")
+    public Optional<Book> getBook(@PathParam("id") int id) {
+        logger.info("Return a single book with id " + id);
+        return bookRepository.getAllBooks().stream().filter(book -> book.id == id).findFirst();
+    }
+}
